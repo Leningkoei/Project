a041ed3f5ca1bd93dd4217fd6392e7a5282f4a61
Leningkoei <zhanglening2001@gmail.com>
2021-03-21 17:24:01 +0900

    例2.4

--- C++/School/DataStructureTutorial/Example/02/04_0/04_0/main.cpp
+++ C++/School/DataStructureTutorial/Example/02/04_0/04_0/main.cpp
@@ -9,48 +9,60 @@ int main()
 	int list1[10] = { 5, 231, 24, 4354, 451, 426, 433, 314, 6457, 73 };
 	int list2[10] = { 543, 54255, 80, 4544, 452, 62, 134, 143, 5423, 3421 };
 	
-	SqList L0_0;
-	SqList L0_1;
+	SqList* L0_0 = new SqList;
+	SqList* L0_1 = new SqList;
 
-	SqList L1_0;
-	SqList L1_1;
+	SqList* L1_0 = new SqList;
+	SqList* L1_1 = new SqList;
 
-	SqList L2_0;
-	SqList L2_1;
+	SqList* L2_0 = new SqList;
+	SqList* L2_1 = new SqList;
 
-	L0_0.creat_list(list0);
-	L0_0.partition1();
+	L0_0->creat_list(list0);
+	L0_0->partition1();
 	cout << "way 0:" << endl;
-	L0_0.display_list();
-	
-	L0_1.creat_list(list0);
-	L0_1.partition2();
-	cout << "way 1:" << endl;
-	L0_1.display_list();
+	L0_0->display_list();
+	delete L0_0;
 
 	cout << endl;
 	
-	L1_0.creat_list(list1);
-	L1_0.partition1();
-	cout << "way 0:" << endl;
-	L1_0.display_list();
-
-	L1_1.creat_list(list1);
-	L1_1.partition2();
+	L0_1->creat_list(list0);
+	L0_1->partition2();
 	cout << "way 1:" << endl;
-	L1_1.display_list();
+	L0_1->display_list();
+	delete L0_1;
+
+	cout << endl;
+	
+	L1_0->creat_list(list1);
+	L1_0->partition1();
+	cout << "way 0:" << endl;
+	L1_0->display_list();
+	delete L1_0;
+
+	cout << endl;
+
+	L1_1->creat_list(list1);
+	L1_1->partition2();
+	cout << "way 1:" << endl;
+	L1_1->display_list();
+	delete L1_1;
 	
 	cout << endl;
 
-	L2_0.creat_list(list2);
-	L2_0.partition1();
+	L2_0->creat_list(list2);
+	L2_0->partition1();
 	cout << "way 0:" << endl;
-	L2_0.display_list();
+	L2_0->display_list();
+	delete L2_0;
 
-	L2_1.creat_list(list2);
-	L2_1.partition2();
+	cout << endl;
+
+	L2_1->creat_list(list2);
+	L2_1->partition2();
 	cout << "way 1:" << endl;
-	L2_1.display_list();
+	L2_1->display_list();
+	delete L2_1;
 
 	return 0;
 }
