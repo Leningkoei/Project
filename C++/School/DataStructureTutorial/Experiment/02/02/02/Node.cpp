1b43349bed501612afe0b76bffdf94d685f3aabd
Leningkoei <zhanglening2001@gmail.com>
2021-03-25 01:10:42 +0900

    数据结构教程第2章实验02

--- C++/School/DataStructureTutorial/Experiment/02/02/02/Node.cpp
+++ C++/School/DataStructureTutorial/Experiment/02/02/02/Node.cpp
@@ -1,5 +1,13 @@
 #include "Node.h"
 #include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+Node::~Node()
+{
+	cout << "É¾³ýthis nodeÁËÅ¶" << endl;
+}
 
 Node::Node()
 {
@@ -11,4 +19,16 @@ Node::Node(char node_data)
 {
 	data = node_data;
 	next_node_ip = NULL;
+}
+
+void display_node_data(Node* p)
+{
+	if (p == NULL)
+	{
+		cout << "this node²»´æÔÚ°¡" << endl;
+	}
+	else
+	{
+		cout << p->data << endl;
+	}
 }
