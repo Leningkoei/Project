808d1e65381e29601d00cfa8d229277451c8c72e
Leningkoei <zhanglening2001@gmail.com>
2021-03-26 16:05:08 +0900

    搜索插入位置

--- C#/School/WindowsProgramming/ArraysAndString/InsertLocation/InsertLocation/Program.cs
+++ C#/School/WindowsProgramming/ArraysAndString/InsertLocation/InsertLocation/Program.cs
@@ -10,7 +10,8 @@ namespace InsertLocation
             int target = 5;
             Solution solution = new();
             int result = solution.SearchInsert(nums, target);
-            Console.WriteLine(result);
+            Console.WriteLine("输入nums = { 1, 3, 5, 6 }, target = 5");
+            Console.WriteLine($"输出target在nums的插入位置 = {result}");
         }
     }
 }
