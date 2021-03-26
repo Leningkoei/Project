2918a6726b0c0428aa257b51007fb1e63f9a160d
Leningkoei <zhanglening2001@gmail.com>
2021-03-26 15:59:59 +0900

    寻找数组的中心索引

---
+++ C#/School/WindowsProgramming/ArraysAndString/ArrayCentralIndex/ArrayCentralIndex/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ArrayCentralIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 7, 3, 6, 5, 6 };
+            int result = Solution.PivotIndex(nums);
+            Console.WriteLine("输入nums = { 1, 7, 3, 6, 5, 6 }");
+            Console.WriteLine($"输出nums = {result}");
+        }
+    }
+}
---
+++ C#/School/WindowsProgramming/ArraysAndString/ArrayCentralIndex/ArrayCentralIndex/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayCentralIndex
+{
+    public class Solution
+    {
+        public static int PivotIndex(int[] nums)
+        {
+            //声明变量
+            int n;
+            int total;
+            int i;
+            int left = 0;
+
+            //求数组元素个数
+            n = nums.Length;
+
+            //求数组元素和
+            total = nums.Sum();
+
+            //遍历数组
+            for (i = 0; i < n; i++)
+            {
+                //如果mid左边之和 + mid右边之和 + mid值 = mid左边之和 * 2 + mid值 == 数组元素和
+                if (left * 2 + nums[i] == total)
+                {
+                    //返回i
+                    return i;
+                }
+                //在遍历中继承出当前的mid左边之和 + 当前mid值, 作为下一个mid左边之和
+                left += nums[i];
+            }
+            //如果Solution没有提前return i, 说明数组不存在中心下标, 返回-1
+            return -1;
+        }
+    }
+}
