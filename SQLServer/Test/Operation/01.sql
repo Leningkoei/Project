c01d438035e223ef0dad2cf7669ee284c61e30db
Leningkoei <zhanglening2001@gmail.com>
2021-04-08 20:56:22 +0900

    数据库系统概论Test

---
+++ SQLServer/Test/Operation/01.sql
@@ -0,0 +1,42 @@
+﻿select
+	Student.s_Name,
+	c_Id_01.c_Id,
+	c_Id_01.s_Score,
+	c_Id_02.c_Id,
+	c_Id_02.s_Score
+from
+	Student,
+	(
+		select
+			Student.s_Id,
+			Score.c_Id,
+			Score.s_Score
+		from
+			Student,
+			Score
+		where
+			Student.s_Id = Score.s_Id
+			and
+			c_Id = '01'
+	)as
+	c_Id_01,
+	(
+		select
+			Student.s_Id,
+			Score.c_Id,
+			Score.s_Score
+		from
+			Student,
+			Score
+		where
+			Student.s_Id = Score.s_Id
+			and
+			c_Id = '02'
+	)as
+	c_Id_02
+where
+	Student.s_Id = c_Id_01.s_Id
+	and
+	Student.s_Id = c_Id_02.s_Id
+	and
+	c_Id_01.s_Score > c_Id_02.s_Score;
---
+++ SQLServer/Test/Operation/04.sql
@@ -0,0 +1,83 @@
+﻿select
+	Student.s_Id,
+	Student.s_Name,
+	/*
+	c_Id_01.c_Id,
+	c_Id_01.s_Score,
+	c_Id_02.c_Id,
+	c_Id_02.s_Score,
+	c_Id_03.c_Id,
+	c_Id_03.s_Score,
+	*/
+	s_AvgScore.s_AvgScore
+from
+	Student,
+	/*
+	(
+		select
+			Student.s_Id,
+			Score.c_Id,
+			Score.s_Score
+		from
+			Student,
+			Score
+		where
+			Student.s_Id = Score.s_Id
+			and
+			Score.c_Id = '01'
+	)as
+	c_Id_01,
+	(
+		select	
+			Student.s_Id,
+			Score.c_Id,
+			Score.s_Score
+		from
+			Student,
+			Score
+		where
+			Student.s_Id = Score.s_Id
+			and
+			Score.c_Id = '02'
+	)as
+	c_Id_02,
+	(
+		select
+			Student.s_Id,
+			Score.c_Id,
+			Score.s_Score
+		from
+			Student,
+			Score
+		where
+			Student.s_Id = Score.s_Id
+			and
+			Score.c_Id = '03'
+	)as
+	c_Id_03,
+	*/
+	(
+		select
+			Score.s_Id,
+			(
+				avg(Score.s_Score)
+			)as
+			s_AvgScore
+		from
+			Score
+		group by
+			Score.s_Id
+	)as
+	s_AvgScore
+where
+	/*
+	Student.s_Id = c_Id_01.s_Id
+	and
+	Student.s_Id = c_Id_02.s_Id
+	and
+	Student.s_Id = c_Id_03.s_Id
+	and
+	*/
+	Student.s_Id = s_AvgScore.s_Id
+	and
+	s_AvgScore.s_AvgScore < 60
