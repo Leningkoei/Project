1b6189453e6a0c82851c640de33f8a45e23c1b82
Leningkoei <zhanglening2001@gmail.com>
2021-04-08 21:39:42 +0900

    Windows程序设计实验0100

---
+++ C#/School/WindowsProgramming/Exprement/01/00/00/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher teacher0 = new Teacher();
+            Student student0 = new Student();
+
+            teacher0.CollectHomework();
+            student0.CollectHomework();
+        }
+    }
+    public class Person
+    {
+
+    }
+    public class Teacher : Person, IHomeworkCollector
+    {
+        public void CollectHomework()
+        {
+            Console.WriteLine("同学们, 请教作业!");
+        }
+    }
+    class Student : Person, IHomeworkCollector
+    {
+        public void CollectHomework()
+        {
+            Console.WriteLine("报告老师! 作业收集完毕!");
+        }
+    }
+    public interface IHomeworkCollector
+    {
+        void CollectHomework();
+    }
+}
