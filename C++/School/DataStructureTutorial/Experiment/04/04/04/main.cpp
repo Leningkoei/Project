e8b6112c1f6a3035a1a5c89db51c2c892cba6a09
Leningkoei <zhanglening2001@gmail.com>
2021-04-16 14:32:51 +0900

    数据结构教程章节04实验04

--- C++/School/DataStructureTutorial/Experiment/04/04/04/main.cpp
+++ C++/School/DataStructureTutorial/Experiment/04/04/04/main.cpp
@@ -19,13 +19,15 @@ int main()
 	string2->add_list(list2, 7);
 	//	加密
 	string3 = string2->get_password_string(string0, string1);
+	delete string2;
 	string3->display_string();
 	//	解密(将加密后的密码串以模板串为标准串, 标准串为模板串进行反向加密得到原始串)
 	string4 = string3->get_password_string(string1, string0);
-	string4->display_string();
-
 	delete string0;
 	delete string1;
+	delete string3;
+	string4->display_string();
+	delete string4;
 
 	return 0;
 }
