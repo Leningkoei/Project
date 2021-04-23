7e14142e8b2a733e7c380bebd88a15740494ce72
Leningkoei <zhanglening2001@gmail.com>
2021-04-23 20:37:24 +0900

    计算方法复杂计算专用解决方案空间

--- Python/Other/ComputingMethod/ComputingMethod/ComputingMethod.py
+++ Python/Other/ComputingMethod/ComputingMethod/ComputingMethod.py
@@ -1,11 +1,1 @@
-x = 1
-for i in range(1, 10):
-    print(i)
-    pre_x = x
-    x = 20 / (x ** 2 + 2 * x + 10)
-    result = x - pre_x
-    print(x)
-    print(result)
-    print("----------------")
-print(7 / 13)
 
