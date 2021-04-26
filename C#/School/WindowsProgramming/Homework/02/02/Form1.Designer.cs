30df0e4d87532d0c5a1223d182a27abf3adc1bf2
Leningkoei <zhanglening2001@gmail.com>
2021-04-26 23:21:10 +0900

    记事本

---
+++ C#/School/WindowsProgramming/Homework/02/02/Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿
+namespace _02
+{
+    partial class Notepade
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnusNotepad = new System.Windows.Forms.MenuStrip();
+            this.tsmiFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiEdit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiFormat = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiView = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiUndo = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmiCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmiCut = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiPaste = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiSelectAll = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmiDate = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiAuto = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiFont = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiStatusStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnusNotepad.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnusNotepad
+            // 
+            this.mnusNotepad.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.mnusNotepad.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiFile,
+            this.tsmiEdit,
+            this.tsmiFormat,
+            this.tsmiView,
+            this.tsmiHelp});
+            this.mnusNotepad.Location = new System.Drawing.Point(0, 0);
+            this.mnusNotepad.Name = "mnusNotepad";
+            this.mnusNotepad.Size = new System.Drawing.Size(584, 25);
+            this.mnusNotepad.TabIndex = 0;
+            this.mnusNotepad.Text = "menuStrip1";
+            // 
+            // tsmiFile
+            // 
+            this.tsmiFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiNew,
+            this.tsmiOpen,
+            this.tsmiSave,
+            this.tsmiSaveAs,
+            this.toolStripSeparator1,
+            this.tsmiClose});
+            this.tsmiFile.ForeColor = System.Drawing.SystemColors.Control;
+            this.tsmiFile.Name = "tsmiFile";
+            this.tsmiFile.Size = new System.Drawing.Size(58, 21);
+            this.tsmiFile.Text = "文件(&F)";
+            // 
+            // tsmiEdit
+            // 
+            this.tsmiEdit.Checked = true;
+            this.tsmiEdit.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.tsmiEdit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiUndo,
+            this.toolStripSeparator2,
+            this.tsmiCopy,
+            this.tsmiCut,
+            this.tsmiPaste,
+            this.toolStripSeparator3,
+            this.tsmiSelectAll,
+            this.tsmiDate});
+            this.tsmiEdit.ForeColor = System.Drawing.SystemColors.Control;
+            this.tsmiEdit.Name = "tsmiEdit";
+            this.tsmiEdit.Size = new System.Drawing.Size(59, 21);
+            this.tsmiEdit.Text = "编辑(&E)";
+            this.tsmiEdit.Click += new System.EventHandler(this.编辑EToolStripMenuItem_Click);
+            // 
+            // tsmiFormat
+            // 
+            this.tsmiFormat.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiAuto,
+            this.tsmiFont});
+            this.tsmiFormat.ForeColor = System.Drawing.SystemColors.Control;
+            this.tsmiFormat.Name = "tsmiFormat";
+            this.tsmiFormat.Size = new System.Drawing.Size(62, 21);
+            this.tsmiFormat.Text = "格式(&O)";
+            this.tsmiFormat.Click += new System.EventHandler(this.tsmiFormat_Click);
+            // 
+            // tsmiView
+            // 
+            this.tsmiView.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiToolStrip,
+            this.tsmiStatusStrip});
+            this.tsmiView.ForeColor = System.Drawing.SystemColors.Control;
+            this.tsmiView.Name = "tsmiView";
+            this.tsmiView.Size = new System.Drawing.Size(60, 21);
+            this.tsmiView.Text = "查看(&V)";
+            // 
+            // tsmiHelp
+            // 
+            this.tsmiHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiAbout});
+            this.tsmiHelp.ForeColor = System.Drawing.SystemColors.Control;
+            this.tsmiHelp.Name = "tsmiHelp";
+            this.tsmiHelp.Size = new System.Drawing.Size(61, 21);
+            this.tsmiHelp.Text = "帮助(&H)";
+            // 
+            // tsmiNew
+            // 
+            this.tsmiNew.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tsmiNew.ForeColor = System.Drawing.SystemColors.Control;
+            this.tsmiNew.Name = "tsmiNew";
+            this.tsmiNew.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.tsmiNew.Size = new System.Drawing.Size(180, 22);
+            this.tsmiNew.Text = "新建(&N)";
+            this.tsmiNew.Click += new System.EventHandler(this.新建NToolStripMenuItem_Click);
+            // 
+            // tsmiOpen
+            // 
+            this.tsmiOpen.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tsmiOpen.ForeColor = System.Drawing.SystemColors.Control;
+            this.tsmiOpen.Name = "tsmiOpen";
+            this.tsmiOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.tsmiOpen.Size = new System.Drawing.Size(180, 22);
+            this.tsmiOpen.Text = "打开(&0)";
+            // 
+            // tsmiSave
+            // 
+            this.tsmiSave.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tsmiSave.ForeColor = System.Drawing.SystemColors.Control;
+            this.tsmiSave.Name = "tsmiSave";
+            this.tsmiSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.tsmiSave.Size = new System.Drawing.Size(180, 22);
+            this.tsmiSave.Text = "保存(&S)";
+            this.tsmiSave.Click += new System.EventHandler(this.保存SToolStripMenuItem_Click);
+            // 
+            // tsmiSaveAs
+            // 
+            this.tsmiSaveAs.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tsmiSaveAs.ForeColor = System.Drawing.SystemColors.Control;
+            this.tsmiSaveAs.Name = "tsmiSaveAs";
+            this.tsmiSaveAs.Size = new System.Drawing.Size(180, 22);
+            this.tsmiSaveAs.Text = "另存为(A)";
+            // 
+            // tsmiClose
+            // 
+            this.tsmiClose.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tsmiClose.ForeColor = System.Drawing.SystemColors.Control;
+            this.tsmiClose.Name = "tsmiClose";
+            this.tsmiClose.Size = new System.Drawing.Size(180, 22);
+            this.tsmiClose.Text = "退出(X)";
+            this.tsmiClose.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // tsmiUndo
+            // 
+            this.tsmiUndo.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tsmiUndo.ForeColor = System.Drawing.SystemColors.Control;
+            this.tsmiUndo.Name = "tsmiUndo";
+            this.tsmiUndo.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Z)));
+            this.tsmiUndo.Size = new System.Drawing.Size(180, 22);
+            this.tsmiUndo.Text = "撤销(&U)";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.toolStripSeparator1.ForeColor = System.Drawing.SystemColors.Control;
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // tsmiCopy
+            // 
+            this.tsmiCopy.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tsmiCopy.ForeColor = System.Drawing.SystemColors.Control;
+            this.tsmiCopy.Name = "tsmiCopy";
+            this.tsmiCopy.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.tsmiCopy.Size = new System.Drawing.Size(180, 22);
+            this.tsmiCopy.Text = "复制(&C)";
+            this.tsmiCopy.Click += new System.EventHandler(this.复制ToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.toolStripSeparator2.ForeColor = System.Drawing.SystemColors.Control;
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // tsmiCut
+            // 
+            this.tsmiCut.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tsmiCut.ForeColor = System.Drawing.SystemColors.Control;
+            this.tsmiCut.Name = "tsmiCut";
+            this.tsmiCut.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.tsmiCut.Size = new System.Drawing.Size(180, 22);
+            this.tsmiCut.Text = "剪切(&T)";
+            // 
+            // tsmiPaste
+            // 
+            this.tsmiPaste.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tsmiPaste.ForeColor = System.Drawing.SystemColors.Control;
+            this.tsmiPaste.Name = "tsmiPaste";
+            this.tsmiPaste.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.tsmiPaste.Size = new System.Drawing.Size(180, 22);
+            this.tsmiPaste.Text = "粘贴(&P)";
+            // 
+            // tsmiSelectAll
+            // 
+            this.tsmiSelectAll.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tsmiSelectAll.ForeColor = System.Drawing.SystemColors.Control;
+            this.tsmiSelectAll.Name = "tsmiSelectAll";
+            this.tsmiSelectAll.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.tsmiSelectAll.Size = new System.Drawing.Size(180, 22);
+            this.tsmiSelectAll.Text = "全选(&A)";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.toolStripSeparator3.ForeColor = System.Drawing.SystemColors.Control;
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(177, 6);
+            // 
+            // tsmiDate
+            // 
+            this.tsmiDate.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tsmiDate.ForeColor = System.Drawing.SystemColors.Control;
+            this.tsmiDate.Name = "tsmiDate";
+            this.tsmiDate.ShortcutKeys = System.Windows.Forms.Keys.F5;
+            this.tsmiDate.Size = new System.Drawing.Size(180, 22);
+            this.tsmiDate.Text = "日期(&D)";
+            this.tsmiDate.Click += new System.EventHandler(this.日期DToolStripMenuItem_Click);
+            // 
+            // tsmiAuto
+            // 
+            this.tsmiAuto.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tsmiAuto.Checked = true;
+            this.tsmiAuto.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.tsmiAuto.ForeColor = System.Drawing.SystemColors.Control;
+            this.tsmiAuto.Name = "tsmiAuto";
+            this.tsmiAuto.Size = new System.Drawing.Size(180, 22);
+            this.tsmiAuto.Text = "自动换行(&W)";
+            // 
+            // tsmiFont
+            // 
+            this.tsmiFont.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tsmiFont.ForeColor = System.Drawing.SystemColors.Control;
+            this.tsmiFont.Name = "tsmiFont";
+            this.tsmiFont.Size = new System.Drawing.Size(180, 22);
+            this.tsmiFont.Text = "字体(&F)";
+            // 
+            // tsmiToolStrip
+            // 
+            this.tsmiToolStrip.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tsmiToolStrip.Checked = true;
+            this.tsmiToolStrip.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.tsmiToolStrip.ForeColor = System.Drawing.SystemColors.Control;
+            this.tsmiToolStrip.Name = "tsmiToolStrip";
+            this.tsmiToolStrip.Size = new System.Drawing.Size(180, 22);
+            this.tsmiToolStrip.Text = "工具栏(&T)";
+            // 
+            // tsmiStatusStrip
+            // 
+            this.tsmiStatusStrip.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tsmiStatusStrip.Checked = true;
+            this.tsmiStatusStrip.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.tsmiStatusStrip.ForeColor = System.Drawing.SystemColors.Control;
+            this.tsmiStatusStrip.Name = "tsmiStatusStrip";
+            this.tsmiStatusStrip.Size = new System.Drawing.Size(180, 22);
+            this.tsmiStatusStrip.Text = "状态栏(&S)";
+            // 
+            // tsmiAbout
+            // 
+            this.tsmiAbout.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tsmiAbout.ForeColor = System.Drawing.SystemColors.Control;
+            this.tsmiAbout.Name = "tsmiAbout";
+            this.tsmiAbout.Size = new System.Drawing.Size(180, 22);
+            this.tsmiAbout.Text = "关于记事本(&A)";
+            // 
+            // Notepade
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(584, 411);
+            this.Controls.Add(this.mnusNotepad);
+            this.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.MainMenuStrip = this.mnusNotepad;
+            this.Name = "Notepade";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "记事本";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.mnusNotepad.ResumeLayout(false);
+            this.mnusNotepad.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnusNotepad;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFile;
+        private System.Windows.Forms.ToolStripMenuItem tsmiEdit;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFormat;
+        private System.Windows.Forms.ToolStripMenuItem tsmiView;
+        private System.Windows.Forms.ToolStripMenuItem tsmiHelp;
+        private System.Windows.Forms.ToolStripMenuItem tsmiNew;
+        private System.Windows.Forms.ToolStripMenuItem tsmiOpen;
+        private System.Windows.Forms.ToolStripMenuItem tsmiSave;
+        private System.Windows.Forms.ToolStripMenuItem tsmiSaveAs;
+        private System.Windows.Forms.ToolStripMenuItem tsmiClose;
+        private System.Windows.Forms.ToolStripMenuItem tsmiUndo;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiCopy;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem tsmiCut;
+        private System.Windows.Forms.ToolStripMenuItem tsmiPaste;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem tsmiSelectAll;
+        private System.Windows.Forms.ToolStripMenuItem tsmiDate;
+        private System.Windows.Forms.ToolStripMenuItem tsmiAuto;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFont;
+        private System.Windows.Forms.ToolStripMenuItem tsmiToolStrip;
+        private System.Windows.Forms.ToolStripMenuItem tsmiStatusStrip;
+        private System.Windows.Forms.ToolStripMenuItem tsmiAbout;
+    }
+}
+
---
+++ C#/School/WindowsProgramming/Homework/02/02/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02
+{
+    public partial class Notepade : Form
+    {
+        public Notepade()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 新建NToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 保存SToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 编辑EToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 复制ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 日期DToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tsmiFormat_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
