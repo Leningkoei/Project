8e2a0e599a78cb47aacb7e2efb20e599d46a108c
Leningkoei <zhanglening2001@gmail.com>
2021-09-24 11:59:01 +0900

    添加了双向多级链表

--- leetcode/classes/ListNode.js
+++ leetcode/classes/ListNode.js
@@ -1,11 +1,27 @@
-export { ListNode };
+export { ListNode, DuoAndSonNode };
 const ListNode = class {
     /**
+     * 单链表;
      * @param {number} val
      * @param {ListNode} next
      */
     constructor(val, next) {
         this.val = (val === undefined) ? 0 : val;
         this.next = next ? next : null;
+    }
+}
+const DuoAndSonNode = class {
+    /**
+     * 双向多级链表;
+     * @param {number} val
+     * @param {DuoAndSonNode} prev
+     * @param {DuoAndSonNode} next
+     * @param {DuoAndSonNode} child
+     */
+    constructor(val, prev, next, child) {
+        this.val = val;
+        this.prev = prev;
+        this.next = next;
+        this.child = child;
     }
 }
