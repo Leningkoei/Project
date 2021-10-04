3d74e1943174ae66f73dd951f2f89bf5702a24f8
Leningkoei <zhanglening2001@gmail.com>
2021-10-04 13:59:58 +0900

    递增的3元子序列

--- leetcode/leetbooks/中级算法/数组/递增的3元子序列.mjs
+++ leetcode/leetbooks/中级算法/数组/递增的3元子序列.mjs
@@ -4,7 +4,18 @@ export { incresingTriplet };
  * @returns {boolean}
  */
 const incresingTriplet = function(nums) {
-    if (nums.length < 3) {
-        return false;
+    if (nums.length > 2) {
+        let small = Infinity;
+        let middle = Infinity;
+        for (const num of nums) {
+            if (num <  small) {
+                small = num;
+            } else if (num > small && num < middle) {
+                middle = num;
+            } else if (num > middle) {
+                return true;
+            }
+        }
     }
+    return false;
 }
