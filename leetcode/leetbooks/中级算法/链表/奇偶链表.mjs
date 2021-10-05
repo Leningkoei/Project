837a502268e5918200d0d25d6a438376566bf880
Leningkoei <zhanglening2001@gmail.com>
2021-10-05 16:33:44 +0900

    这弱智JavaScript and TypeScript插件也太诡异了

--- leetcode/leetbooks/中级算法/链表/奇偶链表.mjs
+++ leetcode/leetbooks/中级算法/链表/奇偶链表.mjs
@@ -1,15 +1,40 @@
-export { oddEvenList };
+export { oddEvenListKai as oddEvenList };
 import { ListNode } from "../../../classes/ListNode.mjs";
 /**
  * @param {ListNode} head
  * @returns {ListNode}
  */
 const oddEvenList = function(head) {
-    const headLog = head;
-    let count = 0;
-    let p = head.next;
-    while (p) {
-        //
+    let p = head;
+    let q = p ? head.next : null;
+    let r = q ? head.next.next : null;
+    const log = head ? head.next : null;
+    while (r) {
+        p.next = r;
+        q.next = r.next;
+        r.next = log;
+        p = p.next;
+        q = q.next;
+        r = q ? q.next : null;
     }
-    return headLog;
+    return head;
+}
+/**
+ * @param {ListNode} head
+ * @returns {ListNode}
+ */
+const oddEvenListKai = function(head) {
+    if (head) {
+        const evenHead = head.next;
+        let odd = head;
+        let even = head.next;
+        while (even && even.next) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+    }
+    return head;
 }
