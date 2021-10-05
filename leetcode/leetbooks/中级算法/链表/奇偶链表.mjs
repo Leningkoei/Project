79f0e31f0401dfc1b4efbcef39f1db51e142e601
Leningkoei <zhanglening2001@gmail.com>
2021-10-05 14:44:35 +0900

    中等

---
+++ leetcode/leetbooks/中级算法/链表/奇偶链表.mjs
@@ -0,0 +1,15 @@
+export { oddEvenList };
+import { ListNode } from "../../../classes/ListNode.mjs";
+/**
+ * @param {ListNode} head
+ * @returns {ListNode}
+ */
+const oddEvenList = function(head) {
+    const headLog = head;
+    let count = 0;
+    let p = head.next;
+    while (p) {
+        //
+    }
+    return headLog;
+}
