031a74806515e46644d927b59bacfdca6461e7ea
Leningkoei <zhanglening2001@gmail.com>
2021-10-08 00:00:02 +0900

    Vue

--- HTML+CSS+JavaScript/background/vue_test/渲染列表/scripts/Vue.mjs
+++ HTML+CSS+JavaScript/background/vue_test/渲染列表/scripts/Vue.mjs
@@ -16,25 +16,65 @@ window.vm = new Vue({
             //     "name": "ccc",
             //     "age": 30
             // } ]
-            "persons": [ 
-                new Person(1, "aaa", 10),
+            "originPersons": [
+                new Person(1, "abc", 10),
                 new Person(2, "bbb", 20),
                 new Person(3, "ccc", 30)
-            ]
+            ],
+            // "persons": [
+            //     // ...this.originPersons
+            //     // new Person(1, "abc", 10),
+            //     // new Person(2, "bbb", 20),
+            //     // new Person(3, "ccc", 30)
+            // ],
+            "keyWord": "",
+            "sortType": 0   //  0: 原顺序, 1: 升序, 2: 降序;
+        }
+    },
+    // "methods": {
+    //     "addNewPerson": function() {
+    //         // const newPerson = {
+    //         //     "id": this.persons[0].id - 1,
+    //         //     "name": "zzz",
+    //         //     "age": 0
+    //         // }
+    //         const newPerson = new Person();
+    //         newPerson.id = this.persons[0].id - 1;
+    //         newPerson.name = "zzz";
+    //         newPerson.age = 0;
+    //         this.persons.unshift(newPerson);
+    //         console.log(this.persons);
+    //     }
+    // },
+    "computed": {
+        "persons": function() {
+            return this.originPersons.filter((person) => person.name.indexOf(this.keyWord) !== -1).sort((a, b) => {
+                switch (this.sortType) {
+                    case 1:
+                    return a.age - b.age;
+                    case 2:
+                    return b.age - a.age;
+                }
+            });
         }
     },
     "methods": {
-        "addNewPerson": function() {
-            // const newPerson = {
-            //     "id": this.persons[0].id - 1,
-            //     "name": "zzz",
-            //     "age": 0
-            // }
-            const newPerson = new Person();
-            newPerson.id = this.persons[0].id - 1;
-            newPerson.name = "zzz";
-            newPerson.age = 0;
-            this.persons.unshift(newPerson);
+        "setZero": function() {
+            this.sortType = 0;
+        },
+        "setOne": function() {
+            this.sortType = 1;
+        },
+        "setTwo": function() {
+            this.sortType = 2;
         }
     }
+    // "watch": {
+    //     "keyWord": {
+    //         "immediate": true,
+    //         "handler": function(value) {
+    //             return this.persons = this.originPersons.filter((person) => person.name.indexOf(value) !== -1);
+    //         }
+    //     }
+    // }
 })
