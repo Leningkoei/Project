67af91827da531743fdce02bd8b783729cff754e
Leningkoei <zhanglening2001@gmail.com>
2021-10-11 21:59:24 +0900

    exprement 00

--- HTML+CSS+JavaScript/test.js
+++ HTML+CSS+JavaScript/test.js
@@ -1,23 +1,2 @@
-const mergeSort = function(nums) {
-    const sort = function(left, right) {
-        if (left < right) {
-            const mid = left + right >> 1;
-            sort(left, mid);
-            sort(mid + 1, right);
-            merge(left, mid, right);
-        }
-    }
-    const merge = function(left, mid, right) {
-        for (let p = mid + 1; p <= right; p++) {
-            for (let q = p; q > left && nums[q - 1] > nums[q]; q--) {
-                console.log(nums[q - 1], nums[q]);
-                result++;
-                [ nums[q - 1], nums[q] ] = [ nums[q], nums[q - 1] ];
-            }
-        }
-    }
-    let result = 0;
-    sort(0, nums.length - 1);
-    console.log(nums);
-    console.log(result);
-}
+import { myFunction } from "../computer_algorithms/exprement/01.mjs";
+window.f = myFunction;
