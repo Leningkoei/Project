d35835ac1396e61090c7914b3721d9a5a667f018
Leningkoei <zhanglening2001@gmail.com>
2021-10-11 21:57:56 +0900

    exprement 00

--- computer_algorithms/exprement/01.mjs
+++ computer_algorithms/exprement/01.mjs
@@ -1,49 +1,67 @@
 export { myFunction };
 function myFunction() {
-    const map = new Set([ 2, 3, 5, 7, 11, 13, 17, 19 ]);
     const grid = new Array(3).fill(undefined).map(() => new Array(3).fill(undefined));
+    const map = new Set([ 2, 3, 5, 7, 11, 13, 17, 19 ]);
     const log = new Set();
     let count = 0;
-    const x = 0;
-    const y = 0;
-    function myFunction1(x, y) {
-        for (let p = 1; p < 11; p++) {
-            if (log.has(p)) {
-                continue;
+    /**
+     * @param {number} x 当前坐标的x值;
+     * @param {number} y 当前坐标的y值;
+     */
+    function dfs(x, y) {
+        if (x === 0 && y === 3) {
+            // 递归出口;
+            count++;
+            console.log("------");
+            for (const row of grid) {
+                console.log(row.toString());
             }
-            grid[y][x] = p;
-            if (delta(x, y)) {
-                log.add(p);
-                if (x === 2) {
-                    if (y === 2) {
-                        count++;
-                        console.log("---------");
-                        for (const row of grid) {
-                            console.log(row.toString());
-                        }
-                    } else {
-                        myFunction1(0, y + 1);
+        } else {
+            for (let thisNum = 1; thisNum < 11; thisNum++) {
+                if (!log.has(thisNum)) {
+                    grid[y][x] = thisNum;
+                    if (delta(x, y)) {
+                        log.add(thisNum);
+                        x === 2 ? dfs(0, y + 1) : dfs(x + 1, y);
+                        log.delete(thisNum);
                     }
-                } else {
-                    myFunction1(x + 1, y);
                 }
-                log.delete(grid[y][x]);
             }
+            grid[y][x] = undefined;
         }
-        grid[y][x] = undefined;
     }
+    /**
+     * 判断grid[y][x]是否符合要求;
+     * @param {number}} x 当前坐标的x值;
+     * @param {number} y 当前坐标的y值;
+     * @returns {boolean}
+     */
     function delta(x, y) {
         const thisNum = grid[y][x];
         const leftNum = getNum(x - 1, y);
         const rightNum = getNum(x + 1, y);
         const upNum = getNum(x, y - 1);
         const downNum = getNum(x, y + 1);
-        const deltaLeftNum = leftNum ? map.has(leftNum + thisNum) : true;
-        const deltaRightNum = rightNum ? map.has(rightNum + thisNum) : true;
-        const deltaUpNum = upNum ? map.has(upNum + thisNum) : true;
-        const deltaDownNum = downNum ? map.has(downNum + thisNum) : true;;
-        return deltaLeftNum && deltaRightNum && deltaUpNum && deltaDownNum;
+        /**
+         * const deltaLeftNum = leftNum ? map.has(leftNum + thisNum) : true;
+         * const deltaRightNum = rightNum ? map.has(rightNum + thisNum) : true;
+         * const deltaUpNum = upNum ? map.has(upNum + thisNum) : true;
+         * const deltaDownNum = downNum ? map.has(downNum + thisNum) : true;
+         * return deltaLeftNum && deltaRightNum && deltaUpNum && deltaDownNum;
+         */
+        return (
+            (leftNum ? map.has(leftNum + thisNum) : true) &&
+            (rightNum ? map.has(rightNum + thisNum) : true) &&
+            (upNum ? map.has(upNum + thisNum) : true) &&
+            (downNum ? map.has(downNum + thisNum) : true)
+        );
     }
+    /**
+     * 获取grid[y][x]的value; -- 若x || y越界, 则返回undefined;
+     * @param {number} x 想要获取value的坐标的x值;
+     * @param {number} y 想要获取value的坐标的y值;
+     * @returns {number}
+     */
     function getNum(x, y) {
         if (y > -1 && y < 3 && x > -1 && x < 3) {
             return grid[y][x];
@@ -51,7 +69,6 @@ function myFunction() {
             return undefined;
         }
     }
-    myFunction1(x, y);
+    dfs(0, 0);
     console.log(count);
 }
-window.f = myFunction;
