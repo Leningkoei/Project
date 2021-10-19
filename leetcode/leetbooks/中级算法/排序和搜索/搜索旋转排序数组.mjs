2369e2d7762e35281d644239f72951822468bc01
Leningkoei <zhanglening2001@gmail.com>
2021-10-19 12:18:49 +0900

    2021/10/18

--- leetcode/leetbooks/中级算法/排序和搜索/搜索旋转排序数组.mjs
+++ leetcode/leetbooks/中级算法/排序和搜索/搜索旋转排序数组.mjs
@@ -1,4 +1,4 @@
-export { search };
+export { searchKai as search };
 /**
  * @param {number[]} nums
  * @param {number} target
@@ -6,29 +6,78 @@ export { search };
  */
 function search(nums, target) {
     let left = 0;
-    let right = nums.length;
-    while (left < right) {
+    let right = nums.length - 1;
+    while (left <= right) {
         const mid = left + right >>> 1;
-        if (mid + 1 === nums.length) {      // 宁可考虑下越界8;
+        if (nums[mid] === nums[right]) {
+            right--;
+        } else if (nums[mid] < nums[right]) {
             right = mid;
-        } else if (nums[mid] > nums[mid + 1]) {
+        } else if (nums[mid] > nums[right]) {
             left = mid + 1;
-        } else if (nums[mid] < nums[mid + 1]) {
-            right = mid;
         }
     }
     // 此时, left === 翻转前的nums[0];
-    const flag = target < nums[0] ? true : false;
-    left = flag ? left : 0;
-    right = flag ? nums.length : left;
+    if (nums[left] === target) {
+        return left;
+    } else if (nums[0] <= target && target <= nums[left - 1]) {
+        right = left;
+        left = 0;
+    } else if (nums[left] < target && target < nums[0]) {
+        right = nums.length;
+        left = left;
+    } else if (nums[left] <= target && target <= nums[nums.length - 1]) {
+        right = nums.length;
+        left = 0;
+    }
     while (left < right) {
         const mid = left + right >>> 1;
         if (nums[mid] === target) {
             return mid;
         } else if (nums[mid] < target) {
-            right = mid;
-        } else if (nums[mid] > target) {
             left = mid + 1;
+        } else if (nums[mid] > target) {
+            right = mid;
+        }
+    }
+    return -1;
+}
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @returns {number}
+ */
+function searchKai(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        const mid = left + right >>> 1;
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[left] === target) {
+            return left;
+        } else if (nums[right] === target) {
+            return right;
+        } else {
+            if (nums[left] <= nums[mid]) {
+                // 此时[left, mid]有序;
+                if (nums[left] < target && target < nums[mid]) {
+                    //此时target在有序区;
+                    right = mid - 1;
+                } else if (nums[mid] < target || target < nums[left]) {
+                    // 此时target在无序区;
+                    left = mid + 1;
+                }
+            } else if (nums[mid] <= nums[right]) {
+                // 此时[mid, right]有序;
+                if (nums[mid] < target && target < nums[right]) {
+                    // 此时target在有序区;
+                    left = mid + 1;
+                } else if (nums[right] < target || target < nums[mid]) {
+                    // 此时target在无序区;
+                    right = mid - 1;
+                }
+            }
         }
     }
     return -1;
