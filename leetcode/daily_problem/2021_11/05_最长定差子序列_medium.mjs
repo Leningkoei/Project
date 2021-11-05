b494de18e85193ed682fa804c502320cd24b42ec
Leningkoei <zhanglening2001@gmail.com>
2021-11-05 22:43:55 +0900

    2021/11/05

--- leetcode/daily_problem/2021_11/05_最长定差子序列_medium.mjs
+++ leetcode/daily_problem/2021_11/05_最长定差子序列_medium.mjs
@@ -8,13 +8,27 @@ function longestSubsequence(arr, difference) {
 
     const map = new Map();
 
-    for (let i = arr.length - 1; i >= 0; i--) {
+    // for (let i = arr.length - 1; i >= 0; i--) {
 
-        const num = arr[i];
-        const preCount = map.get(num + difference);
+    //     const num = arr[i];
+    //     const preCount = map.get(num + difference);
+
+    //     map.set(num, preCount ? preCount + 1 : 1);
+    // }
+
+    for (const num of arr) {
+
+        const preCount = map.get(num - difference);
 
         map.set(num, preCount ? preCount + 1 : 1);
     }
 
     return Math.max(...map.values());
+}
+function longestSubsequenceKai(arr, difference) {
+    return Math.max(...arr.reduce((map, num) => {
+        const preCount = map.get(num - difference);
+        map.set(num, preCount ? preCount + 1 : 1);
+        return map;
+    }, new Map()).values());
 }
