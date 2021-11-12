e32bf8fc32e7587646ee9dc87e6da3e5714f68e4
Leningkoei <zhanglening2001@gmail.com>
2021-11-12 16:38:19 +0900

    2021/11/12

--- leetcode/daily_problem/2021_11/12_猜数字大小II_medium.mjs
+++ leetcode/daily_problem/2021_11/12_猜数字大小II_medium.mjs
@@ -4,14 +4,4 @@ export { getMoneyAmount };
  * @returns {number}
  */
 function getMoneyAmount(n) {
-    switch(true) {
-        case n === 1:
-            return 0;
-        case n === 2:
-            return 1;
-        case n === 3:
-            return 2;
-        default:
-            return n - 3 + Math.max(n - 1, getMoneyAmount(n - 4));
-    }
 }
